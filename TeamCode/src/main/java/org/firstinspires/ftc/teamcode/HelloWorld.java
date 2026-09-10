2d632f1b3a14db889584093f9359b891cdde7ba4
adup-W <dauren-aktau2009@mail.ru>
2026-09-10 19:57:35 +0500

    Create HelloWorld.java
    revising #1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Dauren");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
